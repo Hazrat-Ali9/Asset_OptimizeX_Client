01bf07cd965db708a22c981127d338fd108d0389
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-11-07 11:12:54 +0600

    login :alien:

--- src/components/user/login/login.jsx
+++ src/components/user/login/login.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { storeToken } from "../../../services/localStorageService";
-
+// Login 
 const UserLogin = () => {
     const [userData, setUserData] = useState({});
     const [serverError, setServerError] = useState({})
