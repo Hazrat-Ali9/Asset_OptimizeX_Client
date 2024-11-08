6d53edce38efdb255215925e9174a80a7fe85482
Hazrat-Ali9 <122374893+Hazrat-Ali9@users.noreply.github.com>
2024-11-08 20:54:49 +0600

    main js :alien:

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import App from './App.jsx';
 import { UserProvider } from './contexts/UserContext.jsx';
 import './index.css';
-
+// main js
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
